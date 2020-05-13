d2f0b460e45509f6f1b80b853fbe269847134ef9
Maciej Koch <maciej.k00h@gmail.com>
2020-05-13 12:11:36 +0200

    add distanceOnEartch func

--- fetchPointsToCSV.py
+++ fetchPointsToCSV.py
@@ -1,14 +1,29 @@
 import overpy
 import csv
+from math import sin, cos, sqrt, atan2,radians
 
 
 #   N
 # W + E
 #   S
 
+def distanceOnEarth(x1,x2,y1,y2):
+    R = 6372800  # Earth radius in meters
+
+
+    phi1, phi2 = radians(y1), radians(y2)
+    dphi = radians(lat2 - lat1)
+    dlambda = radians(x2 - x1)
+
+    a = sin(dphi / 2) ** 2 + \
+        cos(phi1) * cos(phi2) * sin(dlambda / 2) ** 2
+
+    return 2 * R * atan2(sqrt(a), sqrt(1 - a))
+
+
 
 api = overpy.Overpass()
-streets =['"Basztowa"', '"Westerplatte"', '"Juliana Dunajewskiego"', '"Podwale"', '"Floriana Straszewskiego"', '"Podzamcze"', '"Świętej Gertrudy"']
+streets =['"Basztowa"']#, '"Juliana Dunajewskiego"', '"Podwale"', '"Floriana Straszewskiego"', '"Podzamcze"', '"Świętego Idziego"', '"Świętej Gertrudy"','"Westerplatte"','"Teatr Słowackiego "']
 row_list = [["id","lon","lat"]]
 
 
@@ -24,6 +39,17 @@ for street in streets:
                 result.ways[i].get_nodes(resolve_missing=True)[j].lon,
                 result.ways[i].get_nodes(resolve_missing=True)[j].lat
             ])
+
+
+
+for i in range(2, len(row_list)):
+    lon1 = row_list[i][1]
+    lat1 = row_list[i][2]
+    lon2 = row_list[i-1][1]
+    lat2 = row_list[i-1][2]
+    print(str(lon2)+ " "+str(lat2)+ " "+ str(distanceOnEarth(lon1,lon2,lat1,lat2)))
+
+
 
 with open('points.csv', 'w', newline='') as file:
     writer = csv.writer(file)
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import overpy
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -15,8 +14,8 @@ x = [ round(lon,7) for lon in df.lon ]
 y = [ round(lat,7) for lat in df.lat ]
 
 #img = plt.imread('./map.png')
-fig, ax = plt.subplots(figsize=(8,10))
-ax.scatter(x, y, zorder=1, alpha= 0.2, c='red', s=10)
+fig, ax = plt.subplots(figsize=(7,9))
+ax.scatter(x, y, zorder=1, alpha= 1, c='red', s=10)
 ax.set_title('I obwodnica Krakowa')
 ax.set_xlim(BBox[0],BBox[1])
 ax.set_ylim(BBox[2],BBox[3])

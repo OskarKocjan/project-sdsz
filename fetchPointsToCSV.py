4fcc5d533aaa2f4115ed07d4bdb78ac5f9e3d8be
Maciej Koch <maciej.k00h@gmail.com>
2020-05-13 12:26:34 +0200

    update

--- fetchPointsToCSV.py
+++ fetchPointsToCSV.py
@@ -1,5 +1,6 @@
 import overpy
 import csv
+import numpy as np
 from math import sin, cos, sqrt, atan2,radians
 
 
@@ -12,7 +13,7 @@ def distanceOnEarth(x1,x2,y1,y2):
 
 
     phi1, phi2 = radians(y1), radians(y2)
-    dphi = radians(lat2 - lat1)
+    dphi = radians(y2 - y1)
     dlambda = radians(x2 - x1)
 
     a = sin(dphi / 2) ** 2 + \
@@ -23,7 +24,7 @@ def distanceOnEarth(x1,x2,y1,y2):
 
 
 api = overpy.Overpass()
-streets =['"Basztowa"']#, '"Juliana Dunajewskiego"', '"Podwale"', '"Floriana Straszewskiego"', '"Podzamcze"', '"Świętego Idziego"', '"Świętej Gertrudy"','"Westerplatte"','"Teatr Słowackiego "']
+streets =['"Basztowa"', '"Juliana Dunajewskiego"', '"Podwale"', '"Floriana Straszewskiego"', '"Podzamcze"', '"Świętego Idziego"', '"Świętej Gertrudy"','"Westerplatte"','"Teatr Słowackiego "']
 row_list = [["id","lon","lat"]]
 
 
@@ -40,14 +41,6 @@ for street in streets:
                 result.ways[i].get_nodes(resolve_missing=True)[j].lat
             ])
 
-
-
-for i in range(2, len(row_list)):
-    lon1 = row_list[i][1]
-    lat1 = row_list[i][2]
-    lon2 = row_list[i-1][1]
-    lat2 = row_list[i-1][2]
-    print(str(lon2)+ " "+str(lat2)+ " "+ str(distanceOnEarth(lon1,lon2,lat1,lat2)))
 
 
 

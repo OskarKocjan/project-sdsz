b70fbd24a823033c40585b70301d2009ff57a9eb
Maciej Koch <maciej.k00h@gmail.com>
2020-05-14 12:36:07 +0200

    update

--- fetchPointsFromCSV.py
+++ fetchPointsFromCSV.py
@@ -1,4 +1,5 @@
 from Point import Point
+from math import sin, cos, sqrt, atan2,radians
 
 def makeDicFromCsv(df):
     points = {}
@@ -9,5 +10,18 @@ def makeDicFromCsv(df):
         for i in range(len(df)):
             points[key].append(Point(round(df.lat[i],7), round(df.lon[i],7)))
     return points
+
+def distance(x1, y1, x2, y2):
+        # Earth radius in meters
+        R = 6372800
+
+        phi1, phi2 = radians(y1), radians(y2)
+        dphi = radians(y2 - y1)
+        dlambda = radians(x2 - x1)
+
+        a = sin(dphi / 2) ** 2 + \
+            cos(phi1) * cos(phi2) * sin(dlambda / 2) ** 2
+
+        return 2 * R * atan2(sqrt(a), sqrt(1 - a))
 
 
